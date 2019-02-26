1fcf12004393f6c916b137f11fee805bfd66f16b
maitungmn <goingdownfr@gmail.com>
2019-02-26 13:22:59 +0700

    done export kpi fe

---
+++ index.js
@@ -0,0 +1,71 @@
+const xl = require('excel4node');
+
+exports.kpiExport = (req, res) => {
+    let headers = req.body.headers;
+    let data = req.body.data;
+    let channelList = req.body.channelList;
+    let ws = [];
+    let wb = new xl.Workbook();
+    let myStyle = wb.createStyle({
+        font: {
+            bold: true,
+            size: 14
+        },
+        alignment: {
+            wrapText: true,
+            horizontal: 'center',
+            vertical: 'center'
+        },
+        border: {
+            left: {
+                style: 'thin',
+                color: 'gray-50'
+            },
+            right: {
+                style: 'thin',
+                color: 'gray-50'
+            },
+            top: {
+                style: 'thin',
+                color: 'gray-50'
+            },
+            bottom: {
+                style: 'thin',
+                color: 'gray-50'
+            },
+            diagonal: {
+                style: 'thin',
+                color: 'gray-50'
+            },
+        },
+        fill: {
+            type: 'pattern',
+            patternType: 'solid',
+            bgColor: 'light yellow',
+            fgColor: 'light yellow'
+        }
+    });
+
+    for (let n = 0; n < channelList.length; n++) {
+        ws[n] = wb.addWorksheet(channelList[n].name);
+
+        for (let i = 1; i <= headers[n].length; i++) {
+            ws[n].cell(1, i)
+                .string(headers[n][i - 1].text)
+                .style(myStyle);
+
+            for (let j = 1; j <= data[n].length; j++) {
+                for (let k = 1; k <= headers[n].length; k++) {
+                    let X = '';
+                    X = String(data[n][j - 1][headers[n][k - 1].value]);
+                    ws[n].cell(j + 1, k).string(X).style({
+                        alignment: {
+                            horizontal: 'right'
+                        }
+                    });
+                }
+            }
+        }
+    }
+    wb.write('kpi_table.xlsx', res);
+};
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Deploy gcloud
+gcloud functions deploy kpiExport --trigger-http --runtime nodejs6 --memory 512MB --project sotetsu-hotel --region asia-northeast1
